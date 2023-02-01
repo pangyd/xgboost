1ae0ccfe211b42a6435fdb3b76cc062da2a8d046
Eternal <842590859@qq.com>
2023-02-01 09:06:45 +0800

    xgboost

--- deploy/remove_gfs.py
+++
@@ -1,8 +0,0 @@
-import os
-import pandas as pd
-import datetime
-import shutil
-
-filename = str((pd.to_datetime(pd.datetime.now()) + datetime.timedelta(days=-3)).strftime("%Y%m%d")) + "12"
-
-shutil.rmtree("/usr/xgboost/GFS_data/{}".format(filename))
